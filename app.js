88afab5af93c8c28909cd9456afa7d74ff8bf9c5
Luis E. Rosa <luis.evandro.rosa@escola.pr.gov.br>
2025-10-09 09:04:38 -0300

    ok

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do número secreto';
+let numeroSecreto = gerarNumeroAleatorio();
 
-let paragrafo = document.querySelector('p')
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = trxto;
+}
 
-
+exibirTextoNaTela('h1', 'Jogo do número secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 
 function verificarChute() {
-    console.log('o botão foi clicado!')
+    console.log('O botão foi clicado!');
+}
+
+function gerarNumeroAleatorio() {
+    parseInt(Math.random() * 10 + 1)
 }
