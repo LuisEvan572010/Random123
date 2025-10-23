b2bbb537332cfa7f63f280e3daf131ada15b62d6
Luis E. Rosa <luis.evandro.rosa@escola.pr.gov.br>
2025-10-23 08:53:48 -0300

    ok

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ let tentativas = 1;
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
-    campo;innerHTML = texto;
+    campo.innerHTML = texto;
     responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});  
 }
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 a 10');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 }
 
 exibirMensagemInicial();
